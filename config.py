9737fe2713b7f68d1426f3db1466ef708fe06ae2
Al-ameen olanrewaju <oadedamola07@gmail.com>
2026-05-26 12:04:58 +0100

    Fix email port

--- config.py
+++ config.py
@@ -27,9 +27,9 @@ class Config:
     FAL_API_KEY = os.environ.get("FAL_KEY")
 
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
